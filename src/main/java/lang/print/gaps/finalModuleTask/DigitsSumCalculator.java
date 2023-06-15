2e97bcc86f64f335e0297b193bd5fd3308bb13db
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 17:17:25 -0400

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int number = 1234;
+int thoun = number/1000;
+int thResult = thoun*1000;
+int sHund =number-thResult;
+int hund = sHund/100;
+int reslHund = hund*100;
+int hResult = sHund-reslHund;
+int decS = hResult/10;
+int decR = decS*10;
+int dRes = hResult-decR;
+ System.out.println(thoun+hund+decS+dRes);
+
     }
 }
