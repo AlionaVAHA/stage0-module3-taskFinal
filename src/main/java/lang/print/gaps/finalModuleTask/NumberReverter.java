09b12470523121da06c1b3c90c46bdeec4a23af6
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 17:16:55 -0400

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+                int hund = number/100;
+        int resHund = hund*100;
+        int dec= number-resHund;
+        int decRes = dec/10;
+        int dResult = decRes*10;
+        int digitRes = dec-dResult;
+        System.out.println(digitRes+""+decRes+""+hund);
+
     }
 }
