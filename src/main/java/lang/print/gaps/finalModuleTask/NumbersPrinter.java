cd40f69f1d3286f419c01dd78a0f747e00e0535e
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-15 18:31:18 -0400

    Update NumbersPrinter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,14 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        boolean positive = true;
-boolean negative=false;
-      if (number >0) {
-          System.out.println(positive);
-      }
-      else{
-          System.out.println(negative);
-      }
+        boolean positive = true ;
+        boolean negative = false;
+        boolean result =(number >0)? positive:negative;
+        System.out.println(result);
 
     }
 }
